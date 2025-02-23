44b607a7758441eebf3327a9940c3b745c81a560
samaffleck <sam.affleck@hotmail.com>
2025-02-23 19:45:51 +0000

    remove build, add gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sam/Documents/cpp/Solvex
-# Build directory: /home/sam/Documents/cpp/Solvex/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
-subdirs("examples")
--- build/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sam/Documents/cpp/Solvex/examples
-# Build directory: /home/sam/Documents/cpp/Solvex/build/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
